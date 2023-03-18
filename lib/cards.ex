2d7c1d71b2750d304bee54070cff9ffac8c71f2f
HimanshuRawat087 <himanshurawat2601@gmail.com>
2023-03-18 15:00:02 +0530

    modified the looping concept

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,16 +1,12 @@
 defmodule Cards do
   def create_deck do
-    values = ["ace" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine" , "ten" , "joker" , "queen" , "king"]
+    values = ["ace" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine" , "ten" , "joker" , "queen"]
     suits = ["spades" , "clubs" , "diamond" ,"heart"]
 
-    total_cards = for suit  <- suits do 
-      for value  <- values do 
+      for value  <- values , suit <- suits do 
         "#{value} of #{suit}"
       end
-    end
-    
-    List.flatten(total_cards)
-  end
+  end 
 
   def shuffle_cards(deck) do
     Enum.shuffle(deck)
