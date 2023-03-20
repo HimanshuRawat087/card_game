aa353105ad9f9903d1d2f0138677df5ef9b828da
HimanshuRawat087 <himanshurawat2601@gmail.com>
2023-03-20 16:22:17 +0530

    added some new function for saving a deck into a file and loading that deck from that file

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,19 +1,38 @@
 defmodule Cards do
   def create_deck do
-    values = ["ace" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine" , "ten" , "joker" , "queen"]
+    values = ["ace" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine" , "ten" , "joker" , "queen" , "king"]
     suits = ["spades" , "clubs" , "diamond" ,"heart"]
 
-      for value  <- values , suit <- suits do 
+      for suit <- suits , value  <- values do
         "#{value} of #{suit}"
       end
-  end 
+  end
 
   def shuffle_cards(deck) do
     Enum.shuffle(deck)
   end
 
-  def contains?(deck , card) do 
+  def contains?(deck , card) do
     Enum.member?(deck , card)
+  end
+
+  def deal(deck , hand_size) do
+    Enum.split(deck , hand_size)
+  end
+
+  def save(deck , filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename , binary)
+  end
+
+  def load(filename) do
+    {status , binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "this file does not exist"
+
+    end
   end
 end
 
